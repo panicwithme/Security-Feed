e88c436f0991213347fcdf95e0a1702633a423c0
panicwithme <fcigdemtosun@protonmail.com>
2021-05-16 20:19:15 +0300

    Add National Vulnerability Database

--- feed.js
+++ feed.js
@@ -1,17 +1,21 @@
 const urls = {
-    'Wired Security'  : 'https://www.wired.com/feed/category/security/latest/rss',
-    'TheHackerNews'   : 'http://feeds.feedburner.com/TheHackersNews?format=xml',
-    'Dark Reading'    : 'https://www.darkreading.com/rss_simple.asp',
-    'KrebsonSecurity' : 'https://krebsonsecurity.com/feed',
-    'ThreatPost'      : 'https://threatpost.com/feed',
+    'Wired Security'                  : 'https://www.wired.com/feed/category/security/latest/rss',
+    'TheHackerNews'                   : 'http://feeds.feedburner.com/TheHackersNews?format=xml',
+    'Dark Reading'                    : 'https://www.darkreading.com/rss_simple.asp',
+    'KrebsonSecurity'                 : 'https://krebsonsecurity.com/feed',
+    'ThreatPost'                      : 'https://threatpost.com/feed',
+    'National Vulnerability Database' : 'https://nvd.nist.gov/feeds/xml/cve/misc/nvd-rss-analyzed.xml',
 };
 
 const renderFeeds = (feeds) => {
+
     const textarea = document.querySelector('#feed-area > ol');
     textarea.value = '';
     document.querySelector('.primary-heading').style.display = 'none';
+    
     feeds.forEach(feed => {
         const date = new Date(feed.date_ms).toLocaleString(),
+        
         li = createEl('li', "list-group-item d-flex justify-content-between align-items-start"),
         divOuter = createEl('div', "ms-2 me-auto"),
         divInner = createEl('div', "fw-bold", `<a href="${feed.link}">${feed.title}</a>`),
@@ -24,12 +28,15 @@ const renderFeeds = (feeds) => {
         li.appendChild(span);
         textarea.appendChild(li);
     });
+
 }
 
 const createEl = (el, className, innerHTML) => {
     let ret = document.createElement(el);
+
     ret.className = className ? className : '';
     ret.innerHTML = innerHTML ? innerHTML : '';
+
     return ret;
 };
 
@@ -41,6 +48,7 @@ const sortByDate = (a, b) => {
 
 (async (urls) => {
     let feeds = [];
+    
     for(el in urls)
         try {
             (await feednami.load(urls[el])).entries.forEach(feed => {
@@ -51,4 +59,5 @@ const sortByDate = (a, b) => {
 
     feeds.sort(sortByDate);
     renderFeeds(feeds);
+
 })(urls);
