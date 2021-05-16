492753db4003db442646f096b8dfd22d30620a11
panicwithme <fcigdemtosun@protonmail.com>
2021-05-16 07:18:59 +0300

    Sort by date

--- feed.js
+++ feed.js
@@ -6,43 +6,58 @@ var urls = {
     'ThreatPost'      : 'https://threatpost.com/feed',
 };
 
+var entries = {};
 const textarea = document.querySelector('#feed-area > ol');
 
-function loadFeed(source, url) {
+function getEntries(source, url) {
     feednami.load(url)
         .then(feed => {
-          textarea.value = '';
-          console.log(feed);
+
           for(let entry of feed.entries) {
-              let date = new Date(entry.date_ms);
-              date = date.toLocaleString();
-
-              let li = document.createElement('li');
-              li.className = "list-group-item d-flex justify-content-between align-items-start";
-              
-              let divOuter = document.createElement('div');
-              divOuter.className = "ms-2 me-auto";
-              
-              let divInner = document.createElement('div');
-              divInner.className = "fw-bold";
-              divInner.innerHTML = `<a href="${entry.link}">${entry.title}</a>`;
-
-              let paragraph = document.createElement('p');
-              paragraph.innerHTML = source;
-              
-              let span = document.createElement('span');
-              span.className = "badge bg-dark rounded-pill";
-              span.innerHTML = date;
-
-              divOuter.appendChild(divInner);
-              divOuter.appendChild(paragraph);
-              li.appendChild(divOuter);
-              li.appendChild(span);
-              textarea.appendChild(li);
+            let entryLink = entry.link;
+            let entryTitle = entry.title;
+            let entryDate = new Date(entry.date_ms);
+            entries[entryDate] = [entryTitle, entryLink, source];
           }
+
         });
 }
 
+function loadEntries(date, info) {
+    textarea.value = '';
+
+    let date = new Date(date);
+    date = date.toLocaleString();
+
+    let li = document.createElement('li');
+    li.className = "list-group-item d-flex justify-content-between align-items-start";
+    
+    let divOuter = document.createElement('div');
+    divOuter.className = "ms-2 me-auto";
+    
+    let divInner = document.createElement('div');
+    divInner.className = "fw-bold";
+    divInner.innerHTML = `<a href="`+info[1]+`">`+info[0]+`</a>`;
+
+    let paragraph = document.createElement('p');
+    paragraph.innerHTML = info[2];
+    
+    let span = document.createElement('span');
+    span.className = "badge bg-dark rounded-pill";
+    span.innerHTML = date;
+
+    divOuter.appendChild(divInner);
+    divOuter.appendChild(paragraph);
+    li.appendChild(divOuter);
+    li.appendChild(span);
+    textarea.appendChild(li);
+}
+
 for (var url_source in urls) {
-    loadFeed(url_source, urls[url_source]);
+    getEntries(url_source, urls[url_source]);
+}
+
+entry_dates = Object.keys(entries).sort();
+for (var entry_date in entries) {
+    loadEntries(entry_date, entries[entry_date]);
 }
