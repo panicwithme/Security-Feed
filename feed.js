b1713ce8d44b23b1782b332224f8ed6a945b7bd9
panicwithme <fcigdemtosun@protonmail.com>
2021-05-16 05:09:25 +0300

    Create URL array, function

--- feed.js
+++ feed.js
@@ -1,26 +1,21 @@
-const url = 'https://www.wired.com/feed/category/security/latest/rss';
-const url2 = 'http://feeds.feedburner.com/TheHackersNews?format=xml';
+var urls = [
+    'https://www.wired.com/feed/category/security/latest/rss',
+    'http://feeds.feedburner.com/TheHackersNews?format=xml'
+];
 
 const textarea = document.querySelector('#feed-area > ul');
 
-feednami.load(url)
-    .then(feed => {
-      textarea.value = ''
-      console.log(feed);
-      for(let entry of feed.entries) {
-          let li = document.createElement('li');
-          li.innerHTML = `<a href="${entry.link}">${entry.title}</a>`;
-          textarea.appendChild(li);
-      }
-    });
+function loadFeed(url) {
+    feednami.load(url)
+        .then(feed => {
+          textarea.value = ''
+          console.log(feed);
+          for(let entry of feed.entries) {
+              let li = document.createElement('li');
+              li.innerHTML = `<a href="${entry.link}">${entry.title}</a>`;
+              textarea.appendChild(li);
+          }
+        });
+}
 
-feednami.load(url2)
-    .then(feed => {
-     textarea.value = ''
-     console.log(feed);
-     for(let entry of feed.entries) {
-         let li = document.createElement('li');
-         li.innerHTML = `<a href="${entry.link}">${entry.title}</a>`;
-	 textarea.appendChild(li);
-     }
-   });
+urls.forEach(loadFeed);
