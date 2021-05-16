10b5ae3e4efb4684b0db640d01a8bc32753cd5f9
panicwithme <fcigdemtosun@protonmail.com>
2021-05-16 05:01:46 +0300

    Add another url

--- feed.js
+++ feed.js
@@ -1,6 +1,7 @@
-const url = 'https://www.wired.com/feed/category/security/latest/rss'
+const url = 'https://www.wired.com/feed/category/security/latest/rss';
+const url2 = 'http://feeds.feedburner.com/TheHackersNews?format=xml';
 
-const textarea = document.querySelector('#feed-area > ul')
+const textarea = document.querySelector('#feed-area > ul');
 
 feednami.load(url)
     .then(feed => {
@@ -12,3 +13,14 @@ feednami.load(url)
           textarea.appendChild(li);
       }
     });
+
+feednami.load(url)
+    .then(feed => {
+     textarea.value = ''
+     console.log(feed);
+     for(let entry of feed.entries) {
+         let li = document.createElement('li');
+         li.innerHTML = `<a href="${entry.link}">${entry.title}</a>`;
+	 textarea.appendChild(li);
+     }
+   });
