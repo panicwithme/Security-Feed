94cab86ca4c196a03580e090f30847a445cc418d
panicwithme <fcigdemtosun@protonmail.com>
2021-05-16 09:49:57 +0300

    Back to source sorting

--- feed.js
+++ feed.js
@@ -6,58 +6,40 @@ var urls = {
     'ThreatPost'      : 'https://threatpost.com/feed',
 };
 
-var entries = {};
 const textarea = document.querySelector('#feed-area > ol');
-
-function getEntries(source, url) {
+function loadFeed(source, url) {
     feednami.load(url)
         .then(feed => {
+          textarea.value = '';
           console.log(feed);
           for(let entry of feed.entries) {
-            let entryLink = entry.link;
-            let entryTitle = entry.title;
-            let entryDate = entry.date_ms;
-            entries[entryDate] = [entryTitle, entryLink, source];
-            console.log(entries[entryDate]);
+              let date = new Date(entry.date_ms);
+              date = date.toLocaleString();
+              let li = document.createElement('li');
+              li.className = "list-group-item d-flex justify-content-between align-items-start";
+
+              let divOuter = document.createElement('div');
+              divOuter.className = "ms-2 me-auto";
+
+              let divInner = document.createElement('div');
+              divInner.className = "fw-bold";
+              divInner.innerHTML = `<a href="${entry.link}">${entry.title}</a>`;
+
+              let paragraph = document.createElement('p');
+              paragraph.innerHTML = source;
+
+              let span = document.createElement('span');
+              span.className = "badge bg-primary rounded-pill";
+              span.innerHTML = date;
+
+              divOuter.appendChild(divInner);
+              divOuter.appendChild(paragraph);
+              li.appendChild(divOuter);
+              li.appendChild(span);
+              textarea.appendChild(li);
           }
         });
 }
-
-function loadEntries(date, info) {
-
-    date = new Date(date);
-    date = date.toLocaleString();
-
-    let li = document.createElement('li');
-    li.className = "list-group-item d-flex justify-content-between align-items-start";
-    
-    let divOuter = document.createElement('div');
-    divOuter.className = "ms-2 me-auto";
-    
-    let divInner = document.createElement('div');
-    divInner.className = "fw-bold";
-    divInner.innerHTML = `<a href="`+info[1]+`">`+info[0]+`</a>`;
-
-    let paragraph = document.createElement('p');
-    paragraph.innerHTML = info[2];
-    
-    let span = document.createElement('span');
-    span.className = "badge bg-dark rounded-pill";
-    span.innerHTML = date;
-
-    divOuter.appendChild(divInner);
-    divOuter.appendChild(paragraph);
-    li.appendChild(divOuter);
-    li.appendChild(span);
-    textarea.appendChild(li);
-}
-
 for (var url_source in urls) {
-    getEntries(url_source, urls[url_source]);
-}
-
-entry_dates = Object.keys(entries).sort();
-textarea.value = '';
-for (var entry_date in entries) {
-    loadEntries(entry_date, entries[entry_date]);
+    loadFeed(url_source, urls[url_source]);
 }
