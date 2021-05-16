a2b9fa443f605323212f3f43c7f48c7cbd68d01c
panicwithme <fcigdemtosun@protonmail.com>
2021-05-16 04:53:53 +0300

    Add ul

--- feed.js
+++ feed.js
@@ -1,12 +1,14 @@
 const url = 'https://www.wired.com/feed/category/security/latest/rss'
 
-const textarea = document.getElementByID('feed-area')
+const textarea = document.querySelector('#feed-area > ul')
 
 feednami.load(url)
     .then(feed => {
       textarea.value = ''
-      console.log(feed)
+      console.log(feed);
       for(let entry of feed.entries) {
-          textarea.value += `${entry.title}\n${entry.link}\n\n`
+          let li = document.createElement('li');
+          li.innerHTML = `<a href="${entry.link}">${entry.title}</a>`;
+          textarea.appendChild(li);
       }
-    })
+    });
